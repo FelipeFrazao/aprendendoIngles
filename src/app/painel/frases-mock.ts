f16df6ca2001f0aabcea792ff2aa6c857c465af7
felipe-frazao <felipe.frazao@al.infnet.edu.br>
2017-10-22 16:59:10 -0200

    Component painel completo, implementada a logica para verificação da frase e trocar a frase

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+export const FRASES: Frase [] = [
+  { fraseIng: 'You are milf' , fraseBr: 'Você é milf'},
+  { fraseIng: 'I watch tv' , fraseBr: 'Eu assisto tv' },
+  { fraseIng: 'Plato is trash, Aristóteles  is god' , fraseBr: 'Platão é lixo, Aristoteles é deus' },
+  { fraseIng: 'I Love Starbucks Frappuccino' , fraseBr: 'Eu amo o frappuccino do Starbucks' }
+];
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,15 +1,45 @@
+// Importando componentes e modulos
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+import {_if} from 'rxjs/observable/if';
+
+// Declarando component
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
+
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase [] = FRASES;
+  public resposta: string;
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+  }
 
   ngOnInit() {
   }
-
+  // Logica para pegar a resposta
+  public pegaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+  // Logica para verificar a resposta digitada
+  public verificaResposta(): void {
+    if (this.resposta === this.rodadaFrase.fraseBr) {
+      console.log('Você acertou meu chapa');
+      // troca a rodada
+      this.rodada++;
+      // atualiza a frase da rodada
+      this.rodadaFrase = this.frases[this.rodada];
+    } else {
+      alert('errou meu chapa');
+    }
+}
 }
