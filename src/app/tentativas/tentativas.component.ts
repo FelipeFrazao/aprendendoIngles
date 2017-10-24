dbbd78cedfcd6180011dfb4dbb39e90303c8b3bf
felipe-frazao <felipe.frazao@al.infnet.edu.br>
2017-10-24 14:20:11 -0200

    Feita a logica para diminuir os corações caso o usuário erre a palavra

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
   public coracaoCheio: string = 'fa fa-heart fa-2x';
   public coracaoVazio: string = 'fa fa-heart-o fa-2x';
@@ -15,6 +15,13 @@ export class TentativasComponent implements OnInit {
     new Coracao(true),  new Coracao(true), new Coracao(true)
   ];
   constructor() {}
+
+  ngOnChanges() {
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false;
+    }
+}
 
   ngOnInit() {
   }
